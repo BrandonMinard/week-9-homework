92b0dddc50a459c46a537936d3c55ccb93007951
Brandon Laptop <brandon.minard@gmail.com>
2021-04-29 04:29:12 -0600

    wrote it recursively

--- index.js
+++ index.js
@@ -188,6 +188,7 @@ const makeReadme = () => {
 //So instead I'm thinking have an escape key, and when you press enter, it just inquires again as long as the escape key wasn't the only entered key.
 //Also can have a key to re-input an answer or something.
 // console.log("Welcome to QuickReadme,\n please follow along and answer questions appropriately to automatically generate a professional readme!\n", "If you feel you made an error in your input,\n please input 'error' in order to redo the previous input.\n If you'd like to skip the current piece of input, type 'skip'")
+console.log("Welcome to QuickReadme,\n please follow along and answer questions appropriately to automatically generate a professional readme!\n", "If you'd like to insert a linebreak, input '-n'", "If you'd like to skip the current piece of input, just don't enter anything and hit ENTER")
 
 function promptObj(message1) {
     return {
@@ -206,6 +207,80 @@ function splitAnswerAndMod(answer) {
     return [answerThing, modifier]
 }
 
+const writingObj = {}
+
+const promptssss = [
+    { prompt: "What's the name of your project?", writeTo: "title", type: "string" },
+    { prompt: "Please describe your project.", writeTo: "description", type: "array" },
+    { prompt: "Describe how to install and import your project", writeTo: "install", type: "array" },
+    { prompt: "Describe how to use your project", writeTo: "usage", type: "array" },
+    { prompt: "license", writeTo: "unneeded", type: "special" },
+    { prompt: "Please describe how someone could contribute to your project.", writeTo: "contributing", type: "array" },
+    { prompt: "Please describe how you tested your project", writeTo: "testing", type: "array" },
+    { prompt: "Please input your github username", writeTo: "github", type: "array" },
+    { prompt: "Please input your email", writeTo: "email", type: "array" }
+
+
+
+]
+let indexer = 0
+recursiveInqExp(promptssss[indexer])
+
+//? chump ? What is your project called?
+
+function recursiveInqExp(objyThing) {
+    indexer++;
+    if (!(objyThing.prompt === "license")) {
+        inquirer.prompt(promptObj(objyThing.prompt)
+        ).then((answers) => {
+            writingObj[objyThing.writeTo] = answers.name;
+        }).finally(() => {
+            if (indexer >= promptssss.length) {
+                console.log(writingObj)
+                return;
+            } else {
+                recursiveInqExp(promptssss[indexer])
+            }
+        });
+    } else {
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'licenses',
+                    message: 'What license would you like to distribute your software under?',
+                    choices: ['MIT License', 'GNU General Public License v3.0', 'The Unlicense'],
+                },
+            ])
+            .then(answers => {
+                if (answers.licenses === 'MIT License') {
+                    console.log("Please copy the file 'LICENSE.txt' into the root folder of your project's directory, you will also have to modify with the current year and your full name.")
+                    writingObj.license = "Licensed under the MIT license, see 'LICENSE.txt' for more information"
+                    writingObj.badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+                } else if (answers.licenses === 'GNU General Public License v3.0') {
+                    console.log("Please copy the file 'COPYING.txt' into the root folder of your project's directory.")
+                    writingObj.license = "Licensed under the GNU General Public License v3.0, see 'COPYING.txt' for more information"
+                    writingObj.badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+                } else if (answers.licenses === 'The Unlicense') {
+                    console.log("Please copy the file 'Unlicense.txt' into the root folder of your project's directory.")
+                    writingObj.license = "Licensed under the Unlicense license, see 'Unlicense.txt' for more information"
+                    writingObj.badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+                }
+            }).finally(() => {
+                if (indexer >= promptssss.length) {
+                    console.log(writingObj)
+                    return;
+                } else {
+                    recursiveInqExp(promptssss[indexer])
+                }
+            });;
+    }
+
+
+}
+
+
+
 // gets name, then starts desription process.
 const init = () => {
     inquirer.prompt(promptObj('What is your project called?')
@@ -215,7 +290,8 @@ const init = () => {
         descriptionStart()
     });
 };
-init()
+// uncomment this
+// init()
 
 
 //Can't methodize, inquirer is far to fragile.
